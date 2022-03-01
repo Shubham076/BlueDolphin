d7fa78544cd39fc597b8c90f21bb4e6179e25905
GitHub Action <action@github.com>
2022-03-01 16:44:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/shubham076/blueocean/MyBundle.kt
+++ src/main/kotlin/com/github/shubham076/blueocean/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.shubham076.blueocean
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/shubham076/blueocean/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shubham076/blueocean/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shubham076.blueocean.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shubham076.blueocean.MyBundle
 
 class MyApplicationService {
 
